b78dd583243c3ab3fd5d8a3d9d76cf25ff992b83
ProjectDerpy <evloika123@live.com>
2018-09-01 15:33:57 -0400

    Update and rename index.js to bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require("config.json");
 const prefix = "-";
 
 
