caa8cc26321ee2b54fe3ee598a014af9960ccad1
ProjectDerpy <evloika123@live.com>
2018-09-01 15:40:42 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("config.json");
 const prefix = "-";
